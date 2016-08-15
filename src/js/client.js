3a0951d78202e4333fc8ae07a673173732209ee5
ebrillhart <emma.brillhart@gmail.com>
2016-08-15 09:37:03 -0700

    add basic bar chart with accessed data

--- src/js/client.js
+++ src/js/client.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * from 'victory';
+import { VictoryBar } from 'victory';
+
+const data = [
+  {quarter: 1, earnings: 13000},
+  {quarter: 2, earnings: 16500},
+  {quarter: 3, earnings: 14250},
+  {quarter: 4, earnings: 19000}
+];
 
 class Main extends React.Component {
   render() {
     return (
       <div>
         <h1>Victory Tutorial</h1>
+        <VictoryBar
+          data={data}
+          x={"quarter"}
+          y={"earnings"}
+        />
       </div>
     );
   }
