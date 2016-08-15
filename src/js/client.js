9bf170061599027e4bd5fcf8128e47adb83c0e98
ebrillhart <emma.brillhart@gmail.com>
2016-08-15 10:03:44 -0700

    adding VictoryStack and additional data

--- src/js/client.js
+++ src/js/client.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
 
-const data = [
+const data2012 = [
   {quarter: 1, earnings: 13000},
   {quarter: 2, earnings: 16500},
   {quarter: 3, earnings: 14250},
   {quarter: 4, earnings: 19000}
+];
+
+const data2013 = [
+  {quarter: 1, earnings: 15000},
+  {quarter: 2, earnings: 12500},
+  {quarter: 3, earnings: 19500},
+  {quarter: 4, earnings: 13000}
+];
+
+const data2014 = [
+  {quarter: 1, earnings: 11500},
+  {quarter: 2, earnings: 13250},
+  {quarter: 3, earnings: 20000},
+  {quarter: 4, earnings: 15500}
+];
+
+const data2015 = [
+  {quarter: 1, earnings: 18000},
+  {quarter: 2, earnings: 13250},
+  {quarter: 3, earnings: 15000},
+  {quarter: 4, earnings: 12000}
 ];
 
 class Main extends React.Component {
@@ -25,11 +46,28 @@ class Main extends React.Component {
             dependentAxis
             tickFormat={(x) => (`$${x / 1000}k`)}
           />
-          <VictoryBar
-            data={data}
-            x={"quarter"}
-            y={"earnings"}
-          />
+          <VictoryStack>
+            <VictoryBar
+              data={data2012}
+              x={"quarter"}
+              y={"earnings"}
+            />
+            <VictoryBar
+              data={data2013}
+              x={"quarter"}
+              y={"earnings"}
+            />
+            <VictoryBar
+              data={data2014}
+              x={"quarter"}
+              y={"earnings"}
+            />
+            <VictoryBar
+              data={data2015}
+              x={"quarter"}
+              y={"earnings"}
+            />
+          </VictoryStack>
         </VictoryChart>
       </div>
     );
