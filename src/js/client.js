c5be2277266d6e78f9402a610decb08e07642de2
ebrillhart <emma.brillhart@gmail.com>
2016-08-15 09:55:46 -0700

    modifying axes and adding domainPadding to chart

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryBar, VictoryChart } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
 
 const data = [
   {quarter: 1, earnings: 13000},
@@ -14,7 +14,16 @@ class Main extends React.Component {
     return (
       <div>
         <h1>Victory Tutorial</h1>
-        <VictoryChart>
+        <VictoryChart
+          domainPadding={10}
+        >
+          <VictoryAxis
+            tickValues={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+          />
+          <VictoryAxis
+            dependentAxis
+            tickFormat={(x) => (`$${x / 1000}k`)}
+          />
           <VictoryBar
             data={data}
             x={"quarter"}
